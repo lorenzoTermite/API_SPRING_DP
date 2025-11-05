537bb5041babef227360192ec8bb6a2bbbb51897
lorenzoTermite <loretermite@gmail.com>
2025-11-05 12:56:41 +0100

    first commit

---
+++ progettotestComponentscan/src/main/java/com/example/progettotest/ProgettoTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.progettotest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoTestApplication.class, args);
+	
+	}
+
+}
---
+++ progettotestComponentscan/src/test/java/com/example/progettotest/ProgettoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progettotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
